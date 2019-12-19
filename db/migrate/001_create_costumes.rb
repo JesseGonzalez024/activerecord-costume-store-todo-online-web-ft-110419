4e3a9ea8445549b83c857605c96ce1f93bc3728b
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-19 18:02:07 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,8 +14,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :image_url
       t.string :size
-      t.timestamps :created_at
-      t.timestamps :updated_at
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
 end
